f950f7c53d9b84f8e8a87c0b6b9fba687996e289
Denis Bekishev <bek2den@gmail.com>
2019-12-14 00:04:47 +0300

    add solution for 6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,8 +1,8 @@
 const Product = require('../models/Product');
-const mongoose = require('mongoose');
 
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
   const query = ctx.query.query;
-  const products = await Product.find({$text: {$search: query}}).sort( {score: {$meta: 'textScore'}} );
+  const products = await Product.find({$text: {$search: query}}, {score: {$meta: 'textScore'}})
+      .sort( {score: {$meta: 'textScore'}} );
   ctx.body = {products};
 };
