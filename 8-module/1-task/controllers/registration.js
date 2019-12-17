b0a68eea6456168148c50b47c4226060249d7e5a
Denis Bekishev <bek2den@gmail.com>
2019-12-17 23:02:58 +0300

    add solution for 8-modules 1-task

--- 8-module/1-task/controllers/registration.js
+++ 8-module/1-task/controllers/registration.js
@@ -3,7 +3,42 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const verificationToken = uuid();
+  let newUser;
+  try {
+    newUser = new User({
+      ...ctx.request.body,
+      verificationToken,
+    });
+    await newUser.setPassword(ctx.request.body.password);
+    await newUser.save();
+
+    sendMail({
+      template: 'confirmation',
+      locals: {token: verificationToken},
+      to: newUser.email,
+      subject: 'Подтвердите почту',
+    });
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = {errors: Object.keys(err.errors).reduce((acc, item) => {
+      acc[item] = err.errors[item].message;
+      return acc;
+    }, {})};
+    return next();
+  }
+
+  ctx.body = {status: 'ok'};
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const {verificationToken} = ctx.request.body;
+  const user = await User.findOne({verificationToken});
+  if (!user) {
+    ctx.status = 400;
+    ctx.body = {error: 'Ссылка подтверждения недействительна или устарела'};
+    return next();
+  }
+  await user.updateOne({$unset: {verificationToken: 1}});
+  ctx.body={token: verificationToken};
 };
