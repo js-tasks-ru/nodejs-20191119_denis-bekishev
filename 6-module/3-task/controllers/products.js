9fc97f516c7f5bb0176044e3639dfe68acc8d56e
Denis Bekishev <bek2den@gmail.com>
2019-12-17 13:45:42 +0300

    add solution for 6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -2,6 +2,8 @@ const Product = require('../models/Product');
 
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
   const query = ctx.query.query;
+  if (!query) return next();
+
   const products = await Product
       .find(
           {$text: {$search: query}},
