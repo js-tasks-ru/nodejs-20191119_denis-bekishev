a3c6c421a74eea12d3ec5fed59f96738564f1b63
Denis Bekishev <bek2den@gmail.com>
2019-12-18 01:36:44 +0300

    add solution for 8-modules 2-task

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -1,8 +1,27 @@
 const Order = require('../models/Order');
+const Product = require('../models/Product');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const order = new Order({
+    ...ctx.request.body,
+    user: ctx.user._id,
+  });
+
+  await order.save();
+  const product = await Product.findOne({_id: order.product});
+
+  await sendMail({
+    to: ctx.user.email,
+    subject: 'Подтвердите почту',
+    locals: {id: order._id, product},
+    template: 'order-confirmation',
+  });
+
+  ctx.body = {order: order._id};
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const orders = await Order.find({user: ctx.user._id}).populate('product');
+  ctx.body = {orders};
 };
